35d63cda95435417fe8fdd217b74287b9f738dc2
voboron1 <voboron1@J08-38.uhk.cz>
2016-11-09 12:03:26 +0100

    Background movement add

--- src/game/GamePlate.java
+++ src/game/GamePlate.java
@@ -1,7 +1,58 @@
 package game;
 
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
 public class GamePlate extends JPanel{
 	public static final int WIDTH = 600, HEIGHT = 800;
+	
+	//rychlost behu pozadi
+	public static final int SPEED = -2;
+	
+	//TODO
+	
+	
+	
+	private BufferedImage img;
+	
+	private Timer animationTimer;
+	
+	private boolean isPaused = false;
+	private boolean isRunning = true;
+	
+	private int BackgroundMoveX = 0;
+	
+	
+	public GamePlate(){
+		
+		
+	}
+	
+	public void paint (Graphics g){
+		super.paint(g);
+		
+		g.drawImage(img, BackgroundMoveX, 0, null);
+		
+		g.drawImage(img, BackgroundMoveX+img.getWidth(), 0, null);
+		
+	}
+	
+	private void update(){
+		if( !isPaused && isRunning){
+			
+			//TODO
+			
+			BackgroundMoveX = BackgroundMoveX + GamePlate.SPEED;
+			
+			if(BackgroundMoveX == -img.getWidth()){
+				BackgroundMoveX = 0;
+			}
+		}
+		
+		
+	}
+	
 }
